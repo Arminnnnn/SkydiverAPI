b88aca62e4a2cc4958c2e56f34b185dff9afe7fc
Armin Joldic <Armin Joldic@DESKTOP-B2IUSQK>
2022-05-16 20:29:51 +0200

    Projektdateien hinzufügen.

---
+++ Skydiver/Models/SkydiverDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Skydiver.Models
+{
+    public class SkydiverDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string SkydiverCollectionName { get; set; } = null!;
+    }
+}
---
+++ Skydiver/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Skydiver.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string UserName { get; set; } = null!;
+
+        public decimal UserHighscore { get; set; }
+    }
+}
